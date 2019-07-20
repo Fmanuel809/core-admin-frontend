e88abffa79a4192ee1af90f83d4e5671f1843c61
Felix M. Martinez S <felixmanuel396@gmail.com>
2019-07-20 02:13:55 -0400

    build 0.1 -- Dashboard

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import {
@@ -9,7 +11,15 @@ import {
     NotificationComponent,
     ChatComponent
 } from './components';
-import { StatModule } from '../../shared';
+import { StatModule, PageHeaderModule } from '../../shared';
+
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
+
+import { ChartsRoutingModule } from '../charts/charts-routing.module';
+import { ChartsComponent } from '../charts/charts.component';
+
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { SiteActivityComponent } from './components/site-activity/site-activity.component';
 
 @NgModule({
     imports: [
@@ -17,13 +27,20 @@ import { StatModule } from '../../shared';
         NgbCarouselModule,
         NgbAlertModule,
         DashboardRoutingModule,
-        StatModule
+        StatModule,
+        AngularFontAwesomeModule,
+        Ng2Charts,
+        ChartsRoutingModule,
+        PageHeaderModule
     ],
     declarations: [
         DashboardComponent,
         TimelineComponent,
         NotificationComponent,
-        ChatComponent
+        ChatComponent,
+        UserInfoComponent,
+        SiteActivityComponent,
+        ChartsComponent
     ]
 })
 export class DashboardModule {}
