0f8bd3619fe12813996a79189db67f6b288bf216
Felix Martinez <felixmanuel396@gmail.com>
2019-08-05 17:22:16 -0400

    JWT API Auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
+import { AuthService } from "../services/auth/auth.service";
 
 @Component({
     selector: 'app-login',
@@ -9,13 +10,18 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
+
+    username: string = '';
+    password: string = '';
+
     constructor(
-      public router: Router
+      public router: Router,
+      private authService: AuthService
     ) {}
 
     ngOnInit() {}
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
+    Login() {
+        this.authService.login(this.username, this.password);
     }
 }
